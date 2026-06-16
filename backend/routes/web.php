dd32ffb4bce18cc9e1b7ba9e7a726d8a3c003dac
kazuma01 <kazumagt048@gmail.com>
2026-06-16 17:19:10 -0300

    PDF V 0.0.63 ainda nao acabou

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,12 @@
 <?php
+namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\TesteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste', [TesteController::class, 'index']);
